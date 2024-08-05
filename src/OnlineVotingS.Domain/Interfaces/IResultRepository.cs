3877c851607dc2b5ca03fba5ae639075a97ea2c0
amelaabd <amela.abdullahu28@gmail.com>
2024-08-05 16:43:14 +0300

    Interfaces for entities

---
+++ src/OnlineVotingS.Domain/Interfaces/IResultRepository.cs
@@ -0,0 +1,18 @@
+﻿using OnlineVotingS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineVotingS.Domain.Interfaces
+{
+    public interface IResultRepository : IGenericRepository<Result>
+    {
+        Task<IEnumerable<Result>> GetByElectionIDAsync(int electionID);
+
+        Task<IEnumerable<Result>> GetByCandidateIDAsync(int candidateID);
+
+        Task<IEnumerable<Result>> GetRecentResultsAsync(DateTime date);
+    }
+}
---
+++ src/OnlineVotingS.Domain/Interfaces/IVotesRepository.cs
@@ -0,0 +1,20 @@
+﻿using OnlineVotingS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineVotingS.Domain.Interfaces
+{
+    public interface IVotesRepository : IGenericRepository<Votes>
+    {
+        Task<IEnumerable<Votes>> GetByUserIDAsync(int userID);
+
+        Task<IEnumerable<Votes>> GetByElectionIDAsync(int electionID);
+
+        Task<IEnumerable<Votes>> GetByCandidateIDAsync(int candidateID);
+
+        Task<IEnumerable<Votes>> GetRecentVotesAsync(DateTime date);
+    }
+}
