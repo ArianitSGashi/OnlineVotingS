f098900a4144350500ed60d72532e0bf253370a0
bl3rt4 <blerta.aazemi@gmail.com>
2024-08-05 15:56:12 +0200

    feat: error correction

---
+++ src/OnlineVotingS.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using OnlineVotingS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineVotingS.Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        // Retrieves an entity by its unique ID asynchronously
+        Task<T> GetByIdAsync(int id);
+
+        // Retrieves all entities asynchronously
+        Task<IEnumerable<T>> GetAllAsync();
+
+        // Adds a new entity to the repository asynchronously
+        Task AddAsync(T entity);
+
+        // Updates an existing entity in the repository asynchronously
+        Task UpdateAsync(T entity);
+
+        // Deletes an entity by its unique ID asynchronously
+        Task DeleteAsync(int id);
+
+        // Checks if an entity exists by its unique ID asynchronously
+        Task<bool> ExistsAsync(int id);
+    }
+}
--- src/OnlineVotingS.Domain/Interfaces/IResultRepository.cs
+++ src/OnlineVotingS.Domain/Interfaces/IResultRepository.cs
@@ -1,18 +1,20 @@
 ﻿using OnlineVotingS.Domain.Entities;
+using OnlineVotingS.Domain.Entities;
+using OnlineVotingS.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OnlineVotingS.Domain.Interfaces
+namespace OnlineVotingSystem.Domain.Interfaces
 {
     public interface IResultRepository : IGenericRepository<Result>
     {
-        Task<IEnumerable<Result>> GetByElectionIDAsync(int electionID);
+        Task<IEnumerable<Result>> GetByElectionIdAsync(int electionId);
 
-        Task<IEnumerable<Result>> GetByCandidateIDAsync(int candidateID);
+        Task<IEnumerable<Result>> GetByCandidateIdAsync(int candidateId);
 
-        Task<IEnumerable<Result>> GetRecentResultsAsync(DateTime date);
+        Task<IEnumerable<Result>> GetByTotalVotesGreaterThanAsync(int votes);
     }
 }
