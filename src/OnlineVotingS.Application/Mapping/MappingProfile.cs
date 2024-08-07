96b6293e1209a764e30fe37db54149da9e413832
Arianit Gashi <gashiarianit2003@gmail.com>
2024-08-07 13:51:55 +0200

    feat: update entity dto and run migrations about these changes to update the database

--- src/OnlineVotingS.Application/Mapping/MappingProfile.cs
+++ src/OnlineVotingS.Application/Mapping/MappingProfile.cs
@@ -8,23 +8,22 @@ namespace OnlineVotingS.Application.Mapping;
 
     public class MappingProfile : Profile
     {
-        public MappingProfile()
-        {
-        // CreateMap<Source, Destination>();
-        CreateMap<Candidates, CandidatesPostDTO>().ReverseMap();
-        CreateMap<Candidates, CandidatesPutDTO>().ReverseMap();
-        CreateMap<Elections, ElectionsPostDTO>().ReverseMap();
-        CreateMap<Elections, ElectionsPutDTO>().ReverseMap();
-        CreateMap<Votes, VotesPostDTO>().ReverseMap();
-        CreateMap<Votes, VotesPutDTO>().ReverseMap();
-        CreateMap<Result, ResultPostDTO>().ReverseMap();
-        CreateMap<Result, ResultPutDTO>().ReverseMap();
-        CreateMap<Complaints, ComplaintsPostDTO>().ReverseMap();
-        CreateMap<Complaints, ComplaintsPutDTO>().ReverseMap();
-        CreateMap<Campaign, CampaignPostDTO>().ReverseMap();
-        CreateMap<Campaign, CampaignPutDTO>().ReverseMap();
-        CreateMap<Feedback, FeedbackPostDTO>().ReverseMap();
-        CreateMap<Feedback, FeedbackPutDTO>().ReverseMap();
-
-        }
+       public MappingProfile()
+       {
+          // CreateMap<Source, Destination>();
+          CreateMap<Candidates, CandidatesPostDTO>().ReverseMap();
+          CreateMap<Candidates, CandidatesPutDTO>().ReverseMap();
+          CreateMap<Elections, ElectionsPostDTO>().ReverseMap();
+          CreateMap<Elections, ElectionsPutDTO>().ReverseMap();
+          CreateMap<Votes, VotesPostDTO>().ReverseMap();
+          CreateMap<Votes, VotesPutDTO>().ReverseMap();
+          CreateMap<Result, ResultPostDTO>().ReverseMap();
+          CreateMap<Result, ResultPutDTO>().ReverseMap();
+          CreateMap<Complaints, ComplaintsPostDTO>().ReverseMap();
+          CreateMap<Complaints, ComplaintsPutDTO>().ReverseMap();
+          CreateMap<Campaign, CampaignPostDTO>().ReverseMap();
+          CreateMap<Campaign, CampaignPutDTO>().ReverseMap();
+          CreateMap<Feedback, FeedbackPostDTO>().ReverseMap();
+          CreateMap<Feedback, FeedbackPutDTO>().ReverseMap();
+       }
     }
