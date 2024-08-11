898d82dcbc138aa9021c6b88a3d14c951d20c08f
Arianit Gashi <gashiarianit2003@gmail.com>
2024-08-11 20:00:37 +0200

    fix: updated the program.cs file to include AddControllers with views

--- src/OnlineVotingS.API/Program.cs
+++ src/OnlineVotingS.API/Program.cs
@@ -1,16 +1,14 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using OnlineVotingS.Application.Mapping;
 using OnlineVotingS.Infrastructure;
-using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllersWithViews();  // Register MVC services
 builder.Services.AddEndpointsApiExplorer();
 
 // Configure Swagger to include JWT Authentication support
@@ -71,6 +69,8 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
