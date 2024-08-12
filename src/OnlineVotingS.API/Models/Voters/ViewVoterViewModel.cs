75155cadf5c6e64415f3517209b2a2e74e0aae04
Besmir Krasniqi <besmir.krasniqi7@student.uni-pr.edu>
2024-08-12 14:39:33 +0200

    Create the Views about the AddVoter, Edit/DeleteVoter, ViewVoter and create the needed ViewModels in the Model folder.

---
+++ src/OnlineVotingS.API/Models/Voters/ViewVoterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineVotingS.API.Models.Voters
+{
+    public class ViewVoterViewModel
+    {
+        public List<VoterDto> Voters { get; set; } = new List<VoterDto>();
+        public string SearchQuery { get; set; }
+    }
+    public class VoterDto
+    {
+        public string PhotoUrl { get; set; }
+        public string VoterId { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string State { get; set; }
+        public string Parliamentary { get; set; }
+        public string Assembly { get; set; }
+    }
+}
