f4b9c018d0c762c44e88e13d3a6f31b0ad2fcb9c
bl3rt4 <blerta.aazemi@gmail.com>
2024-08-13 11:34:08 +0200

    feat: create show result page

--- src/OnlineVotingS.API/Controllers/AdminController.cs
+++ src/OnlineVotingS.API/Controllers/AdminController.cs
@@ -1,19 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace OnlineVotingS.API.Controllers
+namespace OnlineVotingS.API.Controllers;
+
+public class AdminController : Controller
 {
-    public class AdminController : Controller
+    public IActionResult Dashboard()
     {
-        public IActionResult Dashboard()
-        {
-            return View();
-        }
-
-        public IActionResult Voter()
-        {
-            return View();
-        }
-
-        // Removed GenerateElection and CompleteElection methods
+        return View();
+    }
+    public IActionResult Voter()
+    {
+        return View();
     }
 }
--- src/OnlineVotingS.API/Controllers/ResultController.cs
+++ src/OnlineVotingS.API/Controllers/ResultController.cs
@@ -1,29 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using OnlineVotingS.API.Models;
-
-namespace OnlineVotingS.API.Controllers
+namespace OnlineVotingS.API.Controllers;
+public class ResultController : Controller
 {
-    public class ResultController : Controller
+    public IActionResult ShowResult()
     {
-        public IActionResult ShowResult()
+        var electionList = new List<SelectListItem>
         {
-            var electionList = new List<SelectListItem>
-            {
-                new SelectListItem { Text = "Election 1", Value = "1" },
-                new SelectListItem { Text = "Election 2", Value = "2" }
-            };
-
-            var candidateList = new List<SelectListItem>
-            {
-                new SelectListItem { Text = "Candidate A", Value = "1" },
-                new SelectListItem { Text = "Candidate B", Value = "2" }
-            };
-
-            ViewBag.ElectionList = new SelectList(electionList, "Value", "Text");
-            ViewBag.CandidateList = new SelectList(candidateList, "Value", "Text");
-
-            return View("~/Views/Admin/Result/ShowResult.cshtml"); // Full path to the view
-        }
+            new SelectListItem { Text = "Election 1", Value = "1" },
+            new SelectListItem { Text = "Election 2", Value = "2" }
+        };
+        var candidateList = new List<SelectListItem>
+        {
+            new SelectListItem { Text = "Candidate A", Value = "1" },
+            new SelectListItem { Text = "Candidate B", Value = "2" }
+        };
+        ViewBag.ElectionList = new SelectList(electionList, "Value", "Text");
+        ViewBag.CandidateList = new SelectList(candidateList, "Value", "Text");
+        return View("~/Views/Admin/Result/ShowResult.cshtml"); 
     }
 }
