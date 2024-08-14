38cf14419473e4738dd87ad5799b9c85b4351ca9
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-08-14 14:01:14 +0200

    update

--- src/OnlineVotingS.API/Controllers/VoterController.cs
+++ src/OnlineVotingS.API/Controllers/VoterController.cs
@@ -13,12 +13,13 @@ public class VoterController : Controller
     public IActionResult CandidatePage()
     {
         // This is where you'd pull data from the database.
+
         // For now, we'll mock up some data to illustrate.
         var candidates = new List<CandidateViewModel>
-            {
-                new CandidateViewModel { CandidateID = 1, ElectionID = 101, FullName = "John Doe", Party = "Independent", Description = "Lorem ipsum dolor sit amet.", Income = 50000, Works = "Public services" },
-                new CandidateViewModel { CandidateID = 2, ElectionID = 101, FullName = "Jane Smith", Party = "Democrat", Description = "Consectetur adipiscing elit.", Income = 70000, Works = "Health care" },
-            };
+    {
+        new CandidateViewModel { CandidateID = 1, ElectionID = 101, FullName = "John Doe", Party = "Independent", Description = "Lorem ipsum dolor sit amet.", Income = 50000, Works = "Public services" },
+        new CandidateViewModel { CandidateID = 2, ElectionID = 101, FullName = "Jane Smith", Party = "Democrat", Description = "Consectetur adipiscing elit.", Income = 70000, Works = "Health care" },
+    };
 
         return View(candidates);
     }
