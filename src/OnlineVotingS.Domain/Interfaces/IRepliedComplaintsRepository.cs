6783e53ab028850f2c25466e2ae36c434a23f358
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-08-16 14:51:58 +0200

    feat:create interface for repliedcomplaints into the domain layer, then implement this interface in the infrastructure layer.

---
+++ src/OnlineVotingS.Domain/Interfaces/IRepliedComplaintsRepository.cs
@@ -0,0 +1,15 @@
+﻿using OnlineVotingS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnlineVotingS.Domain.Interfaces;
+
+    public interface IRepliedComplaintsRepository : IGenericRepository<RepliedComplaints>
+    {
+        Task<IEnumerable<RepliedComplaints>> GetByComplaintIDAsync(int complaintID);
+
+        Task<IEnumerable<RepliedComplaints>> GetByReplyTextAsync(string replyText);
+
+        Task<IEnumerable<RepliedComplaints>> GetRecentRepliesAsync(DateTime date);
+    }
