2cad98b09b8401df9788cccddb0f3965bf2becaf
albionasylaj <albionasylajj@gmail.com>
2024-08-19 22:28:31 +0200

    feat: Removed the brackets from namesapces.

--- src/OnlineVotingS.Application/Services/ImplService/ComplaintService.cs
+++ src/OnlineVotingS.Application/Services/ImplService/ComplaintService.cs
@@ -4,74 +4,69 @@ using OnlineVotingS.Application.DTO.PutDTO;
 using OnlineVotingS.Application.Services.IService;
 using OnlineVotingS.Domain.Entities;
 using OnlineVotingS.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace OnlineVotingS.Application.Services.ImplService
+namespace OnlineVotingS.Application.Services.ImplService;
+
+public class ComplaintService : IComplaintService
 {
-    internal class ComplaintService : IComplaintService
+    private readonly IComplaintRepository _complaintRepository;
+    private readonly IMapper _mapper;
+
+    public ComplaintService(IComplaintRepository complaintRepository, IMapper mapper)
     {
-        private readonly IComplaintRepository _complaintRepository;
-        private readonly IMapper _mapper;
+        _complaintRepository = complaintRepository;
+        _mapper = mapper;
+    }
 
-        public ComplaintService(IComplaintRepository complaintRepository, IMapper mapper)
-        {
-            _complaintRepository = complaintRepository;
-            _mapper = mapper;
-        }
-        public async Task<Complaints> CreateComplaintsAsync(ComplaintsPostDTO complaintDto)
-        {
-            var complaint = _mapper.Map<Complaints>(complaintDto);
-            await _complaintRepository.AddAsync(complaint);
-            return complaint;
-        }
+    public async Task<Complaints> CreateComplaintsAsync(ComplaintsPostDTO complaintDto)
+    {
+        var complaint = _mapper.Map<Complaints>(complaintDto);
+        await _complaintRepository.AddAsync(complaint);
+        return complaint;
+    }
 
-        public async Task<bool> DeleteComplaintsAsync(int complaintId)
+    public async Task<bool> DeleteComplaintsAsync(int complaintId)
+    {
+        var exists = await _complaintRepository.ExistsAsync(complaintId);
+        if (!exists)
         {
-            var exists = await _complaintRepository.ExistsAsync(complaintId);
-            if (!exists)
-            {
-                throw new KeyNotFoundException($"Complaint with ID {complaintId} not found.");
-            }
-
-            await _complaintRepository.DeleteAsync(complaintId);
-            return true;
+            throw new KeyNotFoundException($"Complaint with ID {complaintId} not found.");
         }
 
-        public async Task<IEnumerable<Complaints>> GetAllComplaintsAsync()
+        await _complaintRepository.DeleteAsync(complaintId);
+        return true;
+    }
+
+    public async Task<IEnumerable<Complaints>> GetAllComplaintsAsync()
+    {
+        return await _complaintRepository.GetAllAsync();
+    }
+
+    public async Task<IEnumerable<Complaints>> GetComplaintByElectionIdAsync(int electionId)
+    {
+        return await _complaintRepository.GetByElectionIdAsync(electionId);
+    }
+
+    public async Task<Complaints> GetComplaintsByIdAsync(int complaintId)
+    {
+        return await _complaintRepository.GetByIdAsync(complaintId);
+    }
+
+    public async Task<IEnumerable<Complaints>> GetComplaintsByUserIdAsync(string userId)
+    {
+        return await _complaintRepository.GetByUserIdAsync(userId);
+    }
+
+    public async Task<Complaints> UpdateComplaintsAsync(ComplaintsPutDTO complaintDto)
+    {
+        var complaint = await _complaintRepository.GetByIdAsync(complaintDto.ComplaintID);
+        if (complaint == null)
         {
-            return await _complaintRepository.GetAllAsync();
+            throw new KeyNotFoundException($"Complaint with ID {complaintDto.ComplaintID} not found.");
         }
 
-        public async Task<IEnumerable<Complaints>> GetComplaintByElectionIdAsync(int electionId)
-        {
-            return await _complaintRepository.GetByElectionIdAsync(electionId);
-        }
-
-        public async Task<Complaints> GetComplaintsByIdAsync(int complaintId)
-        {
-            return await _complaintRepository.GetByIdAsync(complaintId);
-        }
-
-        public async Task<IEnumerable<Complaints>> GetComplaintsByUserIdAsync(string userId)
-        {
-            return await _complaintRepository.GetByUserIdAsync(userId);
-        }
-
-        public async Task<Complaints> UpdateComplaintsAsync(ComplaintsPutDTO complaintDto)
-        {
-            var complaint = await _complaintRepository.GetByIdAsync(complaintDto.ComplaintID);
-            if (complaint == null)
-            {
-                throw new KeyNotFoundException($"Complaint with ID {complaintDto.ComplaintID} not found.");
-            }
-
-            _mapper.Map(complaintDto, complaint);
-            await _complaintRepository.UpdateAsync(complaint);
-            return complaint;
-        }
+        _mapper.Map(complaintDto, complaint);
+        await _complaintRepository.UpdateAsync(complaint);
+        return complaint;
     }
 }
