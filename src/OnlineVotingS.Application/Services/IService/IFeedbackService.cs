cabde625f7c8a0b133350a22d200268e2dcdb070
Besmir Krasniqi <besmir.krasniqi7@student.uni-pr.edu>
2024-08-19 16:20:05 +0200

    feat: Create the Service Interface and its implementation about the Feedback entity

---
+++ src/OnlineVotingS.Application/Services/IService/IFeedbackService.cs
@@ -0,0 +1,18 @@
+﻿using OnlineVotingS.Application.DTO.PostDTO;
+using OnlineVotingS.Application.DTO.PutDTO;
+using OnlineVotingS.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnlineVotingS.Application.Services.IService
+{
+    public interface IFeedbackService
+    {
+        Task<Feedback> CreateFeedbackAsync(FeedbackPostDTO feedbackDto);
+        Task<Feedback> UpdateFeedbackAsync(FeedbackPutDTO feedbackDto);
+        Task<bool> DeleteFeedbackAsync(int feedbackId);
+        Task<Feedback> GetFeedbackByIdAsync(int feedbackId);
+        Task<IEnumerable<Feedback>> GetAllFeedbacksAsync();
+        Task<IEnumerable<Feedback>> GetFeedbacksByUserIDAsync(string voterId);
+    }
+}
