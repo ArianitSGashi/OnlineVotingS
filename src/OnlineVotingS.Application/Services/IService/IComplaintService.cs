cf1fdb644f7b8a59e595daa6a9bbeae7e21951d7
Albiona <albionasylajj@gmail.com>
2024-08-19 21:44:06 +0200

    feat: Created the Service Interface and its implementation about the Complaints entity

---
+++ src/OnlineVotingS.Application/Services/IService/IComplaintService.cs
@@ -0,0 +1,22 @@
+﻿using OnlineVotingS.Application.DTO.PostDTO;
+using OnlineVotingS.Application.DTO.PutDTO;
+using OnlineVotingS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineVotingS.Application.Services.IService
+{
+    internal interface IComplaintService
+    {
+        Task<Complaints> CreateComplaintsAsync(ComplaintsPostDTO complaintDto);
+        Task<Complaints> UpdateComplaintsAsync(ComplaintsPutDTO complaintDto);
+        Task<bool> DeleteComplaintsAsync(int complaintId);
+        Task<Complaints> GetComplaintsByIdAsync(int complaintId);
+        Task<IEnumerable<Complaints>> GetAllComplaintsAsync();
+        Task<IEnumerable<Complaints>> GetComplaintByElectionIdAsync(int electionId);
+        Task<IEnumerable<Complaints>> GetComplaintsByUserIdAsync(string userId);
+    }
+}
