3f60f884cbafef14e0520b91c2c49eaf5d6a29d7
Besmir Krasniqi <besmir.krasniqi7@student.uni-pr.edu>
2024-08-20 09:58:29 +0200

    feat: Create the Service Interface and its implementation about the Feedback entity

--- src/OnlineVotingS.Application/Services/IService/IFeedbackService.cs
+++ src/OnlineVotingS.Application/Services/IService/IFeedbackService.cs
@@ -5,17 +5,16 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace OnlineVotingS.Application.Services.IService
+namespace OnlineVotingS.Application.Services.IService;
+
+public interface IFeedbackService
 {
-    public interface IFeedbackService
-    {
-        Task<Feedback> CreateFeedbackAsync(FeedbackPostDTO feedbackDto);
-        Task<Feedback> UpdateFeedbackAsync(FeedbackPutDTO feedbackDto);
-        Task<bool> DeleteFeedbackAsync(int feedbackId);
-        Task<Feedback> GetFeedbackByIdAsync(int feedbackId);
-        Task<IEnumerable<Feedback>> GetAllFeedbacksAsync();
-        Task<IEnumerable<Feedback>> GetFeedbacksByUserIDAsync(string voterId);
-        Task<IEnumerable<Feedback>> GetByElectionIDAsync(int electionId);
-        Task<IEnumerable<Feedback>> GetRecentFeedbacksAsync(DateTime date);  
-    }
+    Task<Feedback> CreateFeedbackAsync(FeedbackPostDTO feedbackDto);
+    Task<Feedback> UpdateFeedbackAsync(FeedbackPutDTO feedbackDto);
+    Task<bool> DeleteFeedbackAsync(int feedbackId);
+    Task<Feedback> GetFeedbackByIdAsync(int feedbackId);
+    Task<IEnumerable<Feedback>> GetAllFeedbacksAsync();
+    Task<IEnumerable<Feedback>> GetFeedbacksByUserIDAsync(string voterId);
+    Task<IEnumerable<Feedback>> GetByElectionIDAsync(int electionId);
+    Task<IEnumerable<Feedback>> GetRecentFeedbacksAsync(DateTime date);  
 }
