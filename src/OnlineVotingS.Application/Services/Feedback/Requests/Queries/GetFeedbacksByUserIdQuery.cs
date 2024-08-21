4aa05f1e6999ee6dede385eba5fa8ae2ebef56df
Besmir Krasniqi <besmir.krasniqi7@student.uni-pr.edu>
2024-08-21 14:41:50 +0200

    feat: Switch the Service about the Feedback entity using MediatR Pattern

---
+++ src/OnlineVotingS.Application/Services/Feedback/Requests/Queries/GetFeedbacksByUserIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using OnlineVotingS.Domain.Entities;
+using System.Collections.Generic;
+
+namespace OnlineVotingS.Application.Services.Feedbacks.Requests.Queries;
+
+public class GetFeedbackByUserIdQuery : IRequest<List<Feedback>>
+{
+    public int UserId { get; }
+
+    public GetFeedbackByUserIdQuery(int userId)
+    {
+        UserId = userId;
+    }
+}
+
