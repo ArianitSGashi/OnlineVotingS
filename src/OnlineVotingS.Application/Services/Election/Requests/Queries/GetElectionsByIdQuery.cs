860953f5aabdf1ad01224314b24d4cc79961f566
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-08-22 13:30:10 +0200

    feat: creating GetElectionByIdQuery and GetAllElectionQuery in the Queries folder

---
+++ src/OnlineVotingS.Application/Services/Election/Requests/Queries/GetElectionsByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using OnlineVotingS.Domain.Entities;
+
+namespace OnlineVotingS.Application.Services.Election.Requests.Queries;
+
+public class GetElectionsByIdQuery : IRequest<Elections>
+{
+    public int ElectionID { get; }
+
+    public GetElectionsByIdQuery(int electionId)
+    {
+        ElectionID = electionId;
+    }
+}
