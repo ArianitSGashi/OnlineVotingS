2ac57172130ee3bec85d9bbd8c66c0cc5b6bd1f1
bl3rt4 <blerta.aazemi@gmail.com>
2024-08-22 17:25:19 +0200

    feat: switch the service about the votes entity using mediatR

---
+++ src/OnlineVotingS.Application/Services/Vote/Handlers/Queries/GetAllVotesQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using OnlineVotingS.Application.Services.Vote.Requests.Queries;
+using OnlineVotingS.Domain.Entities;
+using OnlineVotingS.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineVotingS.Application.Services.Vote.Handlers.Queries;
+
+public class GetAllVotesQueryHandler : IRequestHandler<GetAllVotesQuery, IEnumerable<Votes>>
+{
+    private readonly IVotesRepository _votesRepository;
+    private readonly ILogger<GetAllVotesQueryHandler> _logger;
+
+    public GetAllVotesQueryHandler(IVotesRepository votesRepository, ILogger<GetAllVotesQueryHandler> logger)
+    {
+        _votesRepository = votesRepository;
+        _logger = logger;
+    }
+
+    public async Task<IEnumerable<Votes>> Handle(GetAllVotesQuery request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var votes = await _votesRepository.GetAllAsync();
+            _logger.LogInformation("Successfully retrieved {Count} votes.", votes.Count());
+            return votes;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("An error occurred while fetching all votes: {ErrorMessage}", ex.Message);
+            throw;
+        }
+    }
+}
