0f3f99f094bbe9475f399eed2faba468522e0837
bl3rt4 <blerta.aazemi@gmail.com>
2024-08-22 15:38:16 +0200

    feat: correcting syntax errors

--- src/OnlineVotingS.Infrastructure/Repositories/RepliedComplaintsRepository.cs
+++ src/OnlineVotingS.Infrastructure/Repositories/RepliedComplaintsRepository.cs
@@ -7,37 +7,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OnlineVotingS.Infrastructure.Repositories
+namespace OnlineVotingS.Infrastructure.Repositories;
+
+public class RepliedComplaintsRepository : GenericRepository<RepliedComplaints>, IRepliedComplaintsRepository
 {
-    public class RepliedComplaintsRepository : GenericRepository<RepliedComplaints>, IRepliedComplaintsRepository
+    public RepliedComplaintsRepository(ApplicationDbContext context) : base(context)
     {
-        public RepliedComplaintsRepository(ApplicationDbContext context) : base(context)
-        {
-        }
+    }
 
-        public async Task<IEnumerable<RepliedComplaints>> GetByComplaintIDAsync(int complaintID)
-        {
-            return await _dbSet.Where(rc => rc.ComplaintID == complaintID).ToListAsync();
-        }
+    public async Task<IEnumerable<RepliedComplaints>> GetByComplaintIDAsync(int complaintID)
+    {
+        return await _dbSet.Where(rc => rc.ComplaintID == complaintID).ToListAsync();
+    }
 
-        public async Task<IEnumerable<RepliedComplaints>> GetByReplyTextAsync(string replyText)
-        {
-            return await _dbSet.Where(rc => rc.ReplyText.Contains(replyText)).ToListAsync();
-        }
+    public async Task<IEnumerable<RepliedComplaints>> GetByReplyTextAsync(string replyText)
+    {
+        return await _dbSet.Where(rc => rc.ReplyText.Contains(replyText)).ToListAsync();
+    }
 
-        public async Task<IEnumerable<RepliedComplaints>> GetRecentRepliesAsync(DateTime date)
-        {
-            return await _dbSet.Where(rc => rc.ReplyDate >= date).ToListAsync();
-        }
+    public async Task<IEnumerable<RepliedComplaints>> GetRecentRepliesAsync(DateTime date)
+    {
+        return await _dbSet.Where(rc => rc.ReplyDate >= date).ToListAsync();
+    }
 
-        public async Task<List<RepliedComplaints>> GetByUserIdAsync(int userId)
-        {
-            return await _dbSet.Where(rc => rc.userId == userId).ToListAsync();
-        }
+    public async Task<List<RepliedComplaints>> GetByUserIdAsync(int userId)
+    {
+        return await _dbSet.Where(rc => rc.userId == userId).ToListAsync();
+    }
 
-        public async Task<List<RepliedComplaints>> GetByDateRangeAsync(DateTime startDate, DateTime endDate)
-        {
-            return await _dbSet.Where(rc => rc.ReplyDate >= startDate && rc.ReplyDate <= endDate).ToListAsync();
-        }
+    public async Task<List<RepliedComplaints>> GetByDateRangeAsync(DateTime startDate, DateTime endDate)
+    {
+        return await _dbSet.Where(rc => rc.ReplyDate >= startDate && rc.ReplyDate <= endDate).ToListAsync();
     }
 }
