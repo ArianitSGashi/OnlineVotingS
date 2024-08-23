63b1ce8e6947dd1e0e62c6860293a24d435e91a6
albionasylaj <albionasylajj@gmail.com>
2024-08-23 13:52:08 +0200

    feat: created the crud operations for the Complaint controller

--- src/OnlineVotingS.API/Controllers/TempControllers/ComplainController.cs
+++ src/OnlineVotingS.API/Controllers/TempControllers/ComplainController.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using OnlineVotingS.Domain.Entities;
 using OnlineVotingS.API.Models.AdminViewModels.ComplaintViewModels;
+using MediatR;
+using OnlineVotingS.Application.Services.Complaint.Requests.Commands;
+using OnlineVotingS.Application.Services.Complaint.Requests.Queries;
+using OnlineVotingS.Application.DTO.PostDTO;
+using OnlineVotingS.Application.DTO.PutDTO;
 
 namespace OnlineVotingS.API.Controllers.TempControllers;
 
 public class ComplainController : Controller
 {
+    private readonly ISender Mediator;
+    public ComplainController(ISender _mediator)
+    {
+        Mediator = _mediator;
+    }
+
     [HttpGet]
     public IActionResult ViewComplain()
     {
@@ -21,5 +32,69 @@ public class ComplainController : Controller
         // This should match the path in your project
         var model = new ReplyComplaintViewModel(); // Dummy data for now
         return View("~/Views/Admin/Complain/ReplyComplain.cshtml", model);
+    }
+
+    [HttpPost]
+    public async Task<Complaints> Create([FromBody]ComplaintsPostDTO complaintsPost)
+    {
+        var response = await Mediator.Send(new CreateComplaintCommand(complaintsPost));
+
+        return response;
+    }
+
+    [HttpDelete]
+    public async Task<bool> Delete(int ComplaintId)
+    {
+        var response = await Mediator.Send(new DeleteComplaintCommand(ComplaintId));
+
+        return response;
+    }
+
+    [HttpPut]
+    public async Task<Complaints> Update([FromBody] ComplaintsPutDTO complaintsPut)
+    {
+        var response = await Mediator.Send(new UpdateComplaintCommand(complaintsPut));
+
+        return response;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Complaints>> GetAll()
+    {
+        var response = await Mediator.Send(new GetAllComplaintCommand());
+
+        return response;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Complaints>> GetAllByDate(DateTime Date)
+    {
+        var response = await Mediator.Send(new GetByComplaintDateCommand(Date));
+
+        return response;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Complaints>> GetAllByElectionId(int ElectionId)
+    {
+        var response = await Mediator.Send(new GetComplaintByElectionIdCommand(ElectionId));
+
+        return response;
+    }
+
+    [HttpGet]
+    public async Task<Complaints> GetById(int ComplaintId)
+    {
+        var response = await Mediator.Send(new GetComplaintsByIdCommand(ComplaintId));
+
+        return response;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Complaints>> GetComplaintsByUserId(string UserId)
+    {
+        var response = await Mediator.Send(new GetComplaintsByUserIdCommand(UserId));
+
+        return response;
     }
 }
