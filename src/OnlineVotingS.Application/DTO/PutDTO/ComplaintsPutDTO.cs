ba0b333a2fc1a5e5fd1eb263cc4586e6d4eb97e6
Albiona <albionasylajj@gmail.com>
2024-09-30 15:11:02 +0200

    feat: added the necessary validation to the Complain and Reply Complain textfield

--- src/OnlineVotingS.Application/DTO/PutDTO/ComplaintsPutDTO.cs
+++ src/OnlineVotingS.Application/DTO/PutDTO/ComplaintsPutDTO.cs
@@ -8,6 +8,12 @@ public class ComplaintsPutDTO
     public int ComplaintID { get; set; }
     public string UserID { get; set; } = null!;
     public int ElectionID { get; set; }
+
+    [Required(ErrorMessage = "Please enter your complaint.")]
+
+    [StringLength(200, ErrorMessage = "The complaint must be at most 200 characters long.")]
+    
+    [RegularExpression(@"[^+^\-^\/^\*^\(^\)]", ErrorMessage = "Special characters aren't allowed.")] 
     public string ComplaintText { get; set; } = null!;
     public DateTime ComplaintDate { get; set; } = DateTime.UtcNow;
 }
