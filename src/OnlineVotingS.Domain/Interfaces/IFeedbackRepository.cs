0ec4cd6009a972694feae4cd0a754654281b42f9
endihyseni11 <endihyseni2509@gmail.com>
2024-10-07 12:29:18 +0200

    feat:Updated FeedbackRepository according to new changes made to Feedback entity

--- src/OnlineVotingS.Domain/Interfaces/IFeedbackRepository.cs
+++ src/OnlineVotingS.Domain/Interfaces/IFeedbackRepository.cs
@@ -1,13 +1,17 @@
 ﻿using OnlineVotingS.Domain.Entities;
+using OnlineVotingS.Domain.Enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace OnlineVotingS.Domain.Interfaces;
-
-public interface IFeedbackRepository : IGenericRepository<Feedback>
+namespace OnlineVotingS.Domain.Interfaces
 {
-    Task<IEnumerable<Feedback>> GetRecentFeedbacksAsync(DateTime date);
+    public interface IFeedbackRepository : IGenericRepository<Feedback>
+    {
+        // Metoda e re per te marrur feedback bazuar ne Kategori
+        Task<IEnumerable<Feedback>> GetByCategoryAsync(FeedbackCategory category);
+
+        // Metoda per te marr feedback bazuar ne date
+        Task<IEnumerable<Feedback>> GetRecentFeedbacksAsync(DateTime date);
+    }
 }
