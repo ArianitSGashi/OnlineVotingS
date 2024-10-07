a933c92aca7ffc5d992e55fe96f9348aee5e342d
Arianit Gashi <gashiarianit2003@gmail.com>
2024-10-07 22:23:21 +0200

    feat: after feedbackrepository changes update services about feedback entity

---
+++ src/OnlineVotingS.Application/Services/Feedbacks/Requests/Queries/GetFeedbacksByCategoryQuery.cs
@@ -0,0 +1,16 @@
+﻿using FluentResults;
+using MediatR;
+using OnlineVotingS.Domain.Entities;
+using OnlineVotingS.Domain.Enums;
+
+namespace OnlineVotingS.Application.Services.Feedbacks.Requests.Queries;
+
+public class GetFeedbacksByCategoryQuery : IRequest<Result<IEnumerable<Feedback>>>
+{
+    public FeedbackCategory Category { get; }
+
+    public GetFeedbacksByCategoryQuery(FeedbackCategory category)
+    {
+        Category = category;
+    }
+}
--- src/OnlineVotingS.Domain/Interfaces/IFeedbackRepository.cs
+++ src/OnlineVotingS.Domain/Interfaces/IFeedbackRepository.cs
@@ -1,17 +1,11 @@
 ﻿using OnlineVotingS.Domain.Entities;
 using OnlineVotingS.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
-namespace OnlineVotingS.Domain.Interfaces
+namespace OnlineVotingS.Domain.Interfaces;
+
+public interface IFeedbackRepository : IGenericRepository<Feedback>
 {
-    public interface IFeedbackRepository : IGenericRepository<Feedback>
-    {
-        // Metoda e re per te marrur feedback bazuar ne Kategori
-        Task<IEnumerable<Feedback>> GetByCategoryAsync(FeedbackCategory category);
+    Task<IEnumerable<Feedback>> GetByCategoryAsync(FeedbackCategory category);
 
-        // Metoda per te marr feedback bazuar ne date
-        Task<IEnumerable<Feedback>> GetRecentFeedbacksAsync(DateTime date);
-    }
+    Task<IEnumerable<Feedback>> GetRecentFeedbacksAsync(DateTime date);
 }
