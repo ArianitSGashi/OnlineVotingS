9aa13e9bad313fcd764d9f1b5dc8fafd31ce9b38
endihyseni11 <endihyseni2509@gmail.com>
2024-10-07 12:29:18 +0200

    feat:Updated FeedbackRepository according to new changes made to Feedback entity

--- src/OnlineVotingS.Infrastructure/Repositories/FeedbackRepository.cs
+++ src/OnlineVotingS.Infrastructure/Repositories/FeedbackRepository.cs
@@ -1,21 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OnlineVotingS.Domain.Entities;
+using OnlineVotingS.Domain.Enums;
 using OnlineVotingS.Domain.Interfaces;
 using OnlineVotingS.Infrastructure.Persistence.Context;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OnlineVotingS.Infrastructure.Repositories;
-
-public class FeedbackRepository : GenericRepository<Feedback>, IFeedbackRepository
+namespace OnlineVotingS.Infrastructure.Repositories
 {
-    public FeedbackRepository(ApplicationDbContext context) : base(context)
+    public class FeedbackRepository : GenericRepository<Feedback>, IFeedbackRepository
     {
-    }
+        public FeedbackRepository(ApplicationDbContext context) : base(context)
+        {
+        }
 
-    public async Task<IEnumerable<Feedback>> GetRecentFeedbacksAsync(DateTime date)
-    {
-        return await _dbSet.Where(f => f.FeedbackDate >= date).ToListAsync();
+        // Bazuar ne kategori
+        public async Task<IEnumerable<Feedback>> GetByCategoryAsync(FeedbackCategory category)
+        {
+            return await _dbSet.Where(f => f.FeedbackCategory == category).ToListAsync();
+        }
+
+        // Bazuar ne data
+        public async Task<IEnumerable<Feedback>> GetRecentFeedbacksAsync(DateTime date)
+        {
+            return await _dbSet.Where(f => f.FeedbackDate >= date).ToListAsync();
+        }
     }
 }
